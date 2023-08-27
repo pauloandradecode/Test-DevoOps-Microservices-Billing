4bc03592096d723ed02f936c37d6752a72912ca8
Paulo César Andrade González <PauloAndrade@GDLMB005545.local>
2023-08-27 15:37:46 -0600

    .

---
+++ target/generated-sources/annotations/com/paymentchain/billing/common/InvoiceResposeMapperImpl.java
@@ -0,0 +1,48 @@
+package com.paymentchain.billing.common;
+
+import com.paymentchain.billing.dto.InvoiceResponse;
+import com.paymentchain.billing.entities.Invoice;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-08-27T15:36:23-0600",
+    comments = "version: 1.3.1.Final, compiler: Eclipse JDT (IDE) 3.35.0.v20230721-1147, environment: Java 17.0.7 (Eclipse Adoptium)"
+)
+@Component
+public class InvoiceResposeMapperImpl implements InvoiceResposeMapper {
+
+    @Override
+    public InvoiceResponse InvoiceToInvoiceRespose(Invoice source) {
+        if ( source == null ) {
+            return null;
+        }
+
+        InvoiceResponse invoiceResponse = new InvoiceResponse();
+
+        invoiceResponse.setInvoiceId( source.getId() );
+        invoiceResponse.setCustomer( source.getCustomerId() );
+        invoiceResponse.setNumber( source.getNumber() );
+        invoiceResponse.setDetail( source.getDetail() );
+        invoiceResponse.setAmount( source.getAmount() );
+
+        return invoiceResponse;
+    }
+
+    @Override
+    public List<InvoiceResponse> InvoiceListToInvoiceResposeList(List<Invoice> source) {
+        if ( source == null ) {
+            return null;
+        }
+
+        List<InvoiceResponse> list = new ArrayList<InvoiceResponse>( source.size() );
+        for ( Invoice invoice : source ) {
+            list.add( InvoiceToInvoiceRespose( invoice ) );
+        }
+
+        return list;
+    }
+}
